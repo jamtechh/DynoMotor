e618d005c176682d8c773aae1fb1371af354fbf8
JamiulHome <jamiul.haque27@gmail.com>
2025-03-13 22:30:30 -0500

    a

---
+++ build/CMakeFiles/my_demo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_demo.dir/mainProj.cpp.o"
+  "CMakeFiles/my_demo.dir/mainProj.cpp.o.d"
+  "bin/my_demo"
+  "bin/my_demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
